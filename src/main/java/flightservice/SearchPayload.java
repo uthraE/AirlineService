b27e787166d0cbd06790bba2464a5fa1d8eaf488
uellappa <uthrapec181@gmail.com>
2018-08-24 16:07:08 +0530

    issues fixed
    issues fixed

--- src/main/java/flightservice/SearchPayload.java
+++ src/main/java/flightservice/SearchPayload.java
@@ -9,16 +9,16 @@ public class SearchPayload implements Callable{
 	public Object onCall(MuleEventContext eventContext) throws Exception {
 		String flightName = eventContext.getMessage().getInvocationProperty("fName");
 		String flightNumber =  eventContext.getMessage().getInvocationProperty("fNo");
-		String sortBy = eventContext.getMessage().getInvocationProperty("sortBy");
+		//String sortBy = eventContext.getMessage().getInvocationProperty("sortBy");
 		String source = eventContext.getMessage().getInvocationProperty("src");
 		String destination = eventContext.getMessage().getInvocationProperty("dst");
 		System.out.println("flightName  :: " +flightName + "  flightNumber::: " +flightNumber);
 		String query = "SELECT * FROM Flights_Data Where SourceLocation="+source+" and Destination="+destination;
 		// SELECT * FROM Flights_Data Where SourceLocation=#[flowVars.src] and Destination=#[flowVars.dst]
-		boolean isLowtoHigh = false;
 		String payload = "";
+		/*boolean isLowtoHigh = false;
 		if(sortBy == null || sortBy.equalsIgnoreCase("lowPrice"))
-			isLowtoHigh = true;
+			isLowtoHigh = true;*/
 		if(flightName == null && flightNumber == null)
 			payload = query;
 		else if(flightName != null && flightNumber != null)
@@ -28,11 +28,11 @@ public class SearchPayload implements Callable{
 		else if(flightNumber != null)
 			payload = (new StringBuilder(query).append(" and FlightNumber="+flightNumber).toString());
 		System.out.println("payload :::: "+payload);
-		if(isLowtoHigh)
+		/*if(isLowtoHigh)
 			payload = (new StringBuilder(payload).append(" order by Price ASC").toString());
 		else
 			payload = (new StringBuilder(payload).append(" order by Price DESC").toString());
-		System.out.println("Final payload :::: "+payload);
+		System.out.println("Final payload :::: "+payload);*/
 		eventContext.getMessage().setPayload(payload);
 		return eventContext.getMessage().getPayload();
 	}
